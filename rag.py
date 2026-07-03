e089417c75bfe675fe0067209e37dd88c43e4af3
tanmayaande-456 <tanmayaande@gmail.com>
2026-07-03 12:52:44 +0530

    number of chats

--- rag.py
+++ rag.py
@@ -111,6 +111,7 @@ def chunk_pdf(pages, chunk_size=5):
     return chunks
 
 st.sidebar.title("All Chats")
+st.warning(len(st.session_state.chats))
 if files:
     allchunks=[]
     for file in files:
