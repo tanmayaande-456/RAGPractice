3b631b9bb75cf178f84406769ff2476d053d9fb1
tanmayaande-456 <tanmayaande@gmail.com>
2026-07-03 12:49:46 +0530

    all chats sb

--- rag.py
+++ rag.py
@@ -110,9 +110,9 @@ def chunk_pdf(pages, chunk_size=5):
             })
     return chunks
 
-st.warning(st.session_state.chats)
 st.sidebar.title("All Chats")
-st.session_state.chats
+for chat in st.session_state.chats:
+    chat
 if files:
     allchunks=[]
     for file in files:
