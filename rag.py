592a40565eb88987083b9cebf2f9a6ffa7475528
tanmayaande-456 <tanmayaande@gmail.com>
2026-07-03 12:55:13 +0530

    checking session_states

--- rag.py
+++ rag.py
@@ -111,7 +111,7 @@ def chunk_pdf(pages, chunk_size=5):
     return chunks
 
 st.sidebar.title("All Chats")
-st.warning(len(st.session_state.chats))
+print(st.session_state)
 if files:
     allchunks=[]
     for file in files:
