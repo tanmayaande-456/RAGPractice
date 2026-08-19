bf10c249a798c464c1162bd3caf5c7e327137896
tanmayaande-456 <tanmayaande@gmail.com>
2026-08-19 18:09:16 +0530

    embedding model fix

--- rag.py
+++ rag.py
@@ -55,9 +55,10 @@ def get_chroma_collection():
 supabase = get_supabase()
 groq_client = get_groq()
 #test only, remov later!!!
-models = groq_client.models.list()
-st.write([m.id for m in models.data])
+# models = groq_client.models.list()
+# st.write([m.id for m in models.data])
 
+model=get_embedding_model()
 collection = get_chroma_collection()
 st.title("Chatbot")
 
