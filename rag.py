13569c4cfb946ff872c5212ce4198e9430f0edca
tanmayaande-456 <tanmayaande@gmail.com>
2026-08-27 19:38:27 +0530

    Summary debug (chunks)

--- rag.py
+++ rag.py
@@ -362,8 +362,8 @@ def summarize_doc(file):
     if selected_doc is None:
         return "Could not find document"
     doc_id=selected_doc["id"]
-    coll=collection.get(where={"$and": [{"userId": USER_ID}, {"document_id": doc_id}]})
-    st.write("DEBUG document ID:", doc_id)
+    # coll=collection.get(where={"$and": [{"userId": USER_ID}, {"document_id": doc_id}]})
+    coll=collection.get()
     st.write("DEBUG Chroma result:", coll)
     chunks=coll.get("documents", [])
     if not chunks:
